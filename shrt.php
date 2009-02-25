cb7a5ead4a36a03bc2252256fa9307e89691774a
David Logie <davidlogie@mac.com>
2009-02-25 21:36:40 +0000

    Bookmarklet now uses correct URL.

--- shrt.php
+++ shrt.php
@@ -14,6 +14,12 @@ function encode(&$val, $key)
     return $val = urlencode($val);
 }
 
+function url()
+{
+    $protocol = $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
+    return $protocol.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+}
+
 function show_help()
 {
     return isset($_GET['f']) and isset($_GET['c']) and trim($_GET['c']) == HELP_TRIGGER;
@@ -190,7 +196,7 @@ if (isset($_GET['c']) and isset($_GET['f']) and !show_help())
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="custom" id="label" class="out">Shortwave file URL:</label><input type="text" name="custom" value="http://" id="custom" onkeyup="$('link').href=$('link').href.replace(/&f=(.*?)\;/,'&f='+this.value+'\';')">
         </form>
-        <p class="left"><span class="out">bookmarklet: </span><a id="link" href="javascript:shrt();function%20shrt(){var%20nw=false;var%20c=window.prompt('Type%20`help`%20for%20a%20list%20of%20commands:');if(c){if(c.substring(0,1)=='%20'){c=c.replace(/^\s+|\s+$/g,'%20');nw=true;}c=escape(c);var%20u='http://shrt.dev/shrt.php?c='+c+'&f=';if(nw){var%20w=window.open(u);w.focus();}else{window.location.href=u;};};};">shrt</a></p>
+        <p class="left"><span class="out">bookmarklet: </span><a id="link" href="javascript:shrt();function%20shrt(){var%20nw=false;var%20c=window.prompt('Type%20`help`%20for%20a%20list%20of%20commands:');if(c){if(c.substring(0,1)=='%20'){c=c.replace(/^\s+|\s+$/g,'%20');nw=true;}c=escape(c);var%20u='<?php echo url() ?>?c='+c+'&f=';if(nw){var%20w=window.open(u);w.focus();}else{window.location.href=u;};};};">shrt</a></p>
     <?php endif; ?>
 </body>
 </html>
