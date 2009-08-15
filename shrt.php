90b33139d41ba3faec8c87cea4cb95a5453d8a6c
David Logie <davidlogie@mac.com>
2009-08-15 11:15:48 +0100

    Added support for keyword arguments.

--- shrt.php
+++ shrt.php
@@ -1,11 +1,11 @@
 <?php
-define('DEFAULT_DELIMITER', ',');
+define('ARGUMENT_DELIMITER', ',');
 define('DEFAULT_URL', 'http://www.google.com/search?q=%s');
 define('HELP_TITLE', '...your commands');
 define('HELP_TRIGGER', 'help');
 define('SHRT_URL', 'http://github.com/xvzf/shrt/tree/master');
 define('TITLE', '...because Saft is broken in the WebKit nightlies');
-define('USERAGENT', 'Grabbing your shrtcuts. (' . SHRT_URL . '); Allow like Gecko');
+define('USERAGENT', 'Grabbing your shortcuts. (' . SHRT_URL . '); Allow like Gecko');
 define('IS_LOCKED', FALSE);
 define('FILE_MATCH', '');
 
@@ -13,7 +13,7 @@ ini_set('user_agent', USERAGENT);
 
 function encode(&$val, $key)
 {
-    return $val = urlencode($val);
+    return urlencode($val);
 }
 
 function url()
@@ -33,20 +33,44 @@ function title()
     return TITLE;
 }
 
-function get_file($url) 
+function remove_numeric_keys(&$array)
 {
-    if (FILE_MATCH != '')
+    foreach ($array as $key => $value)
     {
-        if (preg_match(FILE_MATCH, $url) == FALSE)
+        if (is_int($key))
         {
-            die("<p><strong class=\"error\">Warning:</strong> The URL <strong>$url</strong> did not match the required pattern.</p>");
+            unset($array[$key]);
         }
+    }
+    return $array;
+
+}
+
+function preg_match_named($pattern, $subject, &$matches, $flags=null, $offset=null)
+{
+    $c = preg_match($pattern, $subject, $matches, $flags, $offset);
+    $matches = remove_numeric_keys($matches);
+    return $c;
+}
+
+function preg_match_all_named($pattern, $subject, &$matches, $flags=null, $offset=null)
+{
+    $c = preg_match_all($pattern, $subject, $matches, $flags, $offset);
+    $matches = remove_numeric_keys($matches);
+    return $c;
+}
+
+function get_file($url)
+{
+    if (FILE_MATCH != '' && preg_match(FILE_MATCH, $url) == FALSE)
+    {
+        die("<p><strong class=\"error\">Warning:</strong> The URL <strong>$url</strong> did not match the required pattern.</p>");
     }
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_USERAGENT, USERAGENT);
+    curl_setopt($ch, CURLOPT_URL, $url);
     $data = curl_exec($ch);
     if(curl_error($ch))
     {
@@ -56,29 +80,67 @@ function get_file($url)
     return $data;
 }
 
-function get_args($arg)
+function get_args_from_command($command)
 {
-    $args = preg_replace('/\s\s+/', ' ', trim($arg));
-    preg_match('/^(?<trigger>(\w|\p{P})+)(\s+(?<terms>.*))?/', $args, $matches);
+    $args = preg_replace('/\s\s+/', ' ', trim($command));
+    preg_match_named('/^(?<trigger>(\w|\p{P})+)(\s+(?<args>.*))?/', $args, $matches);
     if (!$matches){ return; }
-    
-    if (!array_key_exists('terms', $matches))
-    {
-        $matches['terms'] = "";
-    }
-    $terms = explode(DEFAULT_DELIMITER, $matches['terms']);
-    array_walk($terms, 'encode');
 
-	$matches['command'] = $arg;
-    $matches['terms'] = $terms;
+    if (!array_key_exists('args', $matches))
+    {
+        $matches['args'] = "";
+    }
+
+    $arguments = explode(ARGUMENT_DELIMITER, $matches['args']);
+    $kwargs = array();
+
+    $count = 0;
+    foreach ($arguments as $argument)
+    {
+        preg_match_named('/^(?<key>(\w|\p{P})+)=(?<value>.*)$/', $argument, $named_args);
+        if (array_key_exists('key', $named_args))
+        {
+            if ($named_args['key'])
+            {
+                $kwargs[$named_args['key']] = $named_args['value'];
+                unset($arguments[$count]);
+            }
+        }
+        $count++;
+    }
+
+    array_walk($arguments, 'encode');
+
+    $matches['command'] = $command;
+    $matches['args'] = $arguments;
+    $matches['kwargs'] = $kwargs;
     return $matches;
 }
 
-function get_shrts($file)
+function get_shortcut($file, $trigger)
 {
     $file = get_file($file);
     $lines = explode("\n", $file);
-    $shrts = array();
+    foreach($lines as $line)
+    {
+        $line = preg_replace('/\s\s+/', ' ', trim($line));
+        // Kill blank lines, comments
+        if (!preg_match('/^>|\n+/i', $line) && $line != "")
+        {
+            list($s_trigger, $s_url, $s_title) = preg_split('/[ ]+/', $line, 3);
+            if ($trigger == $s_trigger)
+            {
+                return $s_url;
+            }
+        }
+    }
+}
+
+function get_shortcuts($file)
+{
+    $file = get_file($file);
+    $lines = explode("\n", $file);
+    $shortcuts = array();
     foreach ($lines as $line)
     {
         $line = preg_replace('/\s\s+/', ' ', trim($line));
@@ -87,62 +149,94 @@ function get_shrts($file)
         {
             $segments = preg_split('/[ ]+/', $line, 3);
             $takes_search = (strstr($segments[1], "%s") && $segments[0] != "*");
-            $shrts[$segments[0]] = array('trigger' => $segments[0],
-                                         'url' => $segments[1],
-                                         'title' => $segments[2],
-                                         'search' => $takes_search);
+            $shortcuts[$segments[0]] = array('trigger' => $segments[0],
+                                             'url' => $segments[1],
+                                             'title' => $segments[2],
+                                             'search' => $takes_search);
         }
     }
-    return $shrts;
+    return $shortcuts;
 }
 
-function get_url($args, $shrts)
+function get_url($shortcut_url, $command_args)
 {
-	// Referrer
+    // Referrer
     $ref = (!empty($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : "";
     $parsed = parse_url($ref);
     $domain = !empty($parsed['host']) ? $parsed['host'] : "";
+    
+    // all patterns
+    $patterns = array('simple'   => '%[s|d|r]+',
+                      'optional' => '%{.*}',
+                      'kwarg'    => '%{[\w|\p{P}]+:.*}');
 
-	$pattern = "/(%s)|(%{.*})/";
-	// Check if shrt exists
-	if (array_key_exists($args['trigger'], $shrts))
-	{
-	    $shrt = $shrts[$args['trigger']];
-		$url = $shrt['url'];
-		foreach ($args['terms'] as $term)
-		{
-			$url = preg_replace($pattern, $term, $url, 1);
-		}
-	}
-	// Doesn't exist, check for untriggered command
-	else if (array_key_exists('*', $shrts))
-	{
-	    $url = preg_replace($pattern, $args['command'], $shrts['*']);
-        $url = $url['url'];
-	}
-	else
-	{
-	    $url = preg_replace($pattern, $args['command'], DEFAULT_URL);
-	}
-	// Any left over arguments?
-	$url = str_replace('%s', '', $url);
-    preg_match_all("/(?<wrap>%{(?<arg>.*)}$)/", $url, $defaults);
-    $url = str_replace($defaults['wrap'], $defaults['arg'], $url);
-    $url = preg_replace("/%d/", $domain, $url);
-    $url = preg_replace("/%r/", $ref, $url);
-    return $url;
-
+    // build final pattern
+    $pattern = '/';
+    $count = 1;
+    $length = count($patterns);
+    foreach ($patterns as $name => $regex)
+    {
+        $pattern .= '(' . $regex . ')';
+        if ($count != $length)
+        {
+            $pattern .= "|";
+        }
+        $count++;
+    }
+    $pattern .= '/';
+    
+    $parts = preg_split($pattern, $shortcut_url, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+    $shortcut_url = array_shift($parts);
+    
+    $count = 0;
+    foreach ($parts as $part)
+    {
+        if (preg_match('/' . $patterns['kwarg'] . '/', $part))
+        {
+            print "<p>got a kwarg: <code>" . $part . "</p>";
+            if (preg_match_named('/%{(?<key>[\w|\p{P}]+):(?<value>.*)}/', $part, $matches))
+            {
+                $pattern = "/(%s)|(%{.*?})/";
+                if (array_key_exists($matches['key'], $command_args['kwargs']))
+                {
+                    $part = preg_replace($pattern, $command_args['kwargs'][$matches['key']], $matches['value']);
+                    $part = parse_static_args($part, $domain, $ref);
+                    $shortcut_url .= $part;
+                }
+            }
+        }
+        else if (!preg_match('/%{[\w|\p{P}]+:.*}/', $part))
+        {
+            $shortcut_url .= str_replace('%s', $command_args['args'][$count], $part);
+            $count++;
+        }
+    }
+    
+    // replace leftover args
+    preg_match_all_named("/(?<wrap>%{(?<arg>.*)}$)/", $shortcut_url, $defaults);
+    $shortcut_url = str_replace($defaults['wrap'], $defaults['arg'], $shortcut_url);
+    $shortcut_url = parse_static_args($shortcut_url, $domain, $ref);
+    return $shortcut_url;
 }
 
-// Go go gadget shrt!
-if (isset($_GET['c']) and isset($_GET['f']) and !show_help()) 
+function parse_static_args($subject, $domain, $ref)
 {
-    $args = get_args(urldecode($_GET['c']));
-    $shrts = get_shrts(urldecode($_GET['f']));
-	if ($shrts)
-	{
-        header('Location: ' . get_url($args, $shrts));
-	}
+    $subject = preg_replace("/%d/", $domain, $subject);
+    $subject = preg_replace("/%r/", $ref, $subject);
+    return $subject;
+}
+
+function go($command, $file)
+{
+    $args = get_args_from_command($command);
+    $shortcut = get_shortcut($file, $args['trigger']);
+    header('Location: ' . get_url($shortcut, $args));
+}
+
+// Go go gadget shortcut!
+if (isset($_GET['c']) and isset($_GET['f']) and !show_help())
+{
+    go(urldecode($_GET['c']), urldecode($_GET['f']));
 }
 ?>
 <!DOCTYPE html>
@@ -151,7 +245,7 @@ if (isset($_GET['c']) and isset($_GET['f']) and !show_help())
     <meta charset="UTF-8">
     <title>shrt</title>
     <style type="text/css">
-	<?php $color = "#c86f4d"; ?>
+    <?php $color = "#c86f4d"; ?>
     *{margin:0;padding:0;}
     html{background:#fff;border-top:4px solid <?php echo $color; ?>;color:black;font:62.5% Helvetica,sans-serif;text-align:center;}
     body{margin:4em auto;width:50em;}
@@ -183,7 +277,7 @@ if (isset($_GET['c']) and isset($_GET['f']) and !show_help())
 <body>
     <header><h1><a href="<?php echo $_SERVER['SCRIPT_NAME'] ?>">shrt</a> <em><?php echo title(); ?></em></h1></header>
     <?php if (show_help()): ?>
-        <?php $shrts = get_shrts($_GET['f']); ?>
+        <?php $shrts = get_shortcuts($_GET['f']); ?>
         <p><span class="red">*</span> triggers may be followed by a search term. e.g. <code>i stanley kubrick</code></p>
         <table cellspacing="0">
             <thead>
@@ -205,7 +299,7 @@ if (isset($_GET['c']) and isset($_GET['f']) and !show_help())
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="custom" id="label" class="out">Shortwave file URL:</label><input<?php if (IS_LOCKED): ?> disabled="disabled" <?php endif; ?> type="text" name="custom" value="http://" id="custom" onkeyup="$('link').href=$('link').href.replace(/&f=(.*?)\;/,'&f='+this.value+'\';')">
         </form>
-        <p class="left"><span class="out">bookmarklet: </span><a id="link" href="javascript:shrt();function%20shrt(){var%20nw=false;var%20c=window.prompt('Type%20`help`%20for%20a%20list%20of%20commands:');if(c){if(c.substring(0,1)=='%20'){c=c.replace(/^\s+|\s+$/g,'%20');nw=true;}c=escape(c);var%20u='<?php echo url() ?>?c='+c+'&f=';if(nw){var%20w=window.open(u);w.focus();}else{window.location.href=u;};};};" onclick="alert('Drag me onto your browser bookmarks bar.'); return false;">shrt</a></p>
+        <p class="left"><span class="out">bookmarklet: </span><a id="link" href="javascript:shrt();function%20shrt(){var%20nw=false;var%20c=window.prompt('Type%20`help`%20for%20a%20list%20of%20commands:');if(c){if(c.substring(0,1)=='%20'){c=c.replace(/^\s+|\s+$/g,'%20');nw=true;}c=escape(c);var%20u='<?php echo url() ?>?c='+c+'&f=';if(nw){var%20w=window.open(u);w.focus();}else{window.location.href=u;};};};">shrt</a></p>
     <?php endif; ?>
 </body>
 </html>
