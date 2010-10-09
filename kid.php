f7b1cb1f126a65d995673675f2a5a7b0defc26f1
David Logie <davidlogie@mac.com>
2010-10-09 13:27:17 +0100

    New digs.

--- kid.php
+++ kid.php
@@ -7,9 +7,9 @@ define('FILE_MATCH', '', TRUE);
 define('HELP_TITLE', 'your shortcuts', TRUE);
 define('HELP_TRIGGER', 'help', TRUE);
 define('IS_LOCKED', FALSE, TRUE);
-define('NAME', 'shrt', TRUE);
+define('NAME', 'kid', TRUE);
 define('TITLE', 'bookmarklet shortcuts', TRUE);
-define('USERAGENT', 'Grabbing your shortcuts. (http://github.com/xvzf/shrt)', TRUE);
+define('USERAGENT', 'Grabbing your shortcuts. (http://github.com/xvzf/kid)', TRUE);
 
 ini_set('user_agent', USERAGENT);
 
@@ -416,7 +416,7 @@ if (isset($_REQUEST['c']) and isset($_GET['f']))
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="custom" id="label" class="out">shortcuts file:</label><input<?php if (IS_LOCKED): ?> disabled="disabled" <?php endif; ?> type="text" name="custom" value="http://" id="custom">
         </form>
-        <a id="link" href="javascript:shrt();function%20shrt(){var%20nw=false;var%20c=window.prompt('Type%20`<?php echo HELP_TRIGGER ?>`%20for%20a%20list%20of%20commands:');var%20h='';try{h=encodeURIComponent(window.location.hostname);}catch(e){h='about:blank'};var%20u=encodeURIComponent(window.location);var%20t=encodeURIComponent(document.title);if(c){if(c.substring(0,1)=='%20'){nw=true;}c=encodeURIComponent(c);var%20url='<?php echo url() ?>?c='+c+'&f='+'&d='+h+'&r='+u+'&t='+t;if(nw){var%20w=window.open(url);w.focus();}else{window.location.href=url;};};};"><?php echo NAME ?></a>
+        <a id="link" href="javascript:kid();function%20kid(){var%20nw=false;var%20c=window.prompt('Type%20`<?php echo HELP_TRIGGER ?>`%20for%20a%20list%20of%20commands:');var%20h='';try{h=encodeURIComponent(window.location.hostname);}catch(e){h='about:blank'};var%20u=encodeURIComponent(window.location);var%20t=encodeURIComponent(document.title);if(c){if(c.substring(0,1)=='%20'){nw=true;}c=encodeURIComponent(c);var%20url='<?php echo url() ?>?c='+c+'&f='+'&d='+h+'&r='+u+'&t='+t;if(nw){var%20w=window.open(url);w.focus();}else{window.location.href=url;};};};"><?php echo NAME ?></a>
     <?php endif; ?>
 </body>
 </html>
